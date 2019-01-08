a997151e644802b3b17f2f5666a4ed3c3bc47180
DaveP <dave@wearecrystal.uk>
2019-01-08 23:10:07 +0000

    Vue init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
