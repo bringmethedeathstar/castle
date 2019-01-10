8e8580b380d2e8ae301a86a4a3dcb891f05843bd
DaveP <dave@wearecrystal.uk>
2019-01-10 23:17:41 +0000

    input style

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,37 @@ export default {
 <template>
   <div id="app">
     <Float/>
+
+    <h1>castle.</h1>
+
+    <input type="text" placeholder="search me">
   </div>
 </template>
 
-<style>
+<style lang="scss">
+$black: #324048;
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $black;
+  padding: 30px;
+  font-weight: bold;
+}
+
+input {
+  width: 25vw;
+  min-width: 330px;
+  border: 0;
+  border-bottom: 2px solid $black;
+  padding: 10px 15px;
+  text-align: center;
+  font-weight: bold;
+
+  &:focus {
+    outline: none;
+  }
 }
 </style>
