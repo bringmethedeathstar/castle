4dbef0bf35a21af4fc42a698d5e2c405521031d1
DaveP <dave@wearecrystal.uk>
2019-01-14 15:53:19 +0000

    move rightmove logic to correct Host

--- src/components/Host.vue
+++ src/components/Host.vue
@@ -1,6 +1,5 @@
 <script>
 import { mapState } from "vuex";
-import axios from "axios";
 
 import Rightmove from "@/components/hosts/Rightmove";
 
@@ -9,55 +8,7 @@ export default {
     Rightmove
   },
 
-  methods: {
-    async fetch() {
-      let res = await axios.get(
-        "http://api.allorigins.ml/get?url=" + encodeURIComponent(this.address)
-      );
-
-      //itemprop="name"
-      //itemprop="streetAddress"
-      //id="propertyHeaderPrice"
-
-      // <.*itemprop="<thing>".*>([\s\S])</.*>
-
-      console.log(res.data.contents.match(/<body[^>]*>[\s\S]*<\/body>/gi));
-
-      console.log(
-        "title: ",
-        res.data.contents.replace(
-          /[\s\S]*<.*itemprop="name".*>(.*)<\/.*>[\s\S]*/gm,
-          "$1"
-        )
-      );
-
-      console.log(
-        "address: ",
-        res.data.contents.replace(
-          /[\s\S]*<meta itemprop="streetAddress" content="(.*)" \/>[\s\S]*/gm,
-          "$1"
-        )
-      );
-
-      let data = JSON.parse(
-        res.data.contents.replace(/[\s\S]*}\('property',({.*})[\s\S]*/gm, "$1")
-      );
-
-      // console.log(data);
-
-      console.log("postcode: ", data.location.postcode);
-      console.log("price: £", data.propertyInfo.price);
-      console.log("bedrooms: ", data.propertyInfo.beds);
-      console.log("furnished type: ", data.propertyInfo.furnishedType);
-      console.log("propertyId: ", data.propertyId);
-    }
-  },
-
-  computed: mapState(["host", "address"]),
-
-  watch: {
-    address: "fetch"
-  }
+  computed: mapState(["host"])
 };
 </script>
 
--- src/components/hosts/Rightmove.vue
+++ src/components/hosts/Rightmove.vue
@@ -1,11 +1,75 @@
 <script>
-export default {};
+import { mapState, mapMutations } from "vuex";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      place: {}
+    };
+  },
+
+  methods: {
+    async fetch() {
+      try {
+        let res = await axios.get(
+          "http://api.allorigins.ml/get?url=" + encodeURIComponent(this.url)
+        );
+
+        let data = res.data.contents;
+
+        let meta = JSON.parse(
+          data.replace(/[\s\S]*}\('property',({.*})[\s\S]*/gm, "$1")
+        );
+
+        this.place.title = data
+          .replace(/[\s\S]*<.*itemprop="name".*>(.*)<\/.*>[\s\S]*/gm, "$1")
+          .replace("to rent", "")
+          .replace("to let", "");
+
+        this.place.address = data.replace(
+          /[\s\S]*<meta itemprop="streetAddress" content="(.*)" \/>[\s\S]*/gm,
+          "$1"
+        );
+
+        if (data.match(/no pets|pets not/i)) {
+          this.place.pets = false;
+        } else if (
+          data.match(/pets considered|pets allowed|pets permitted|pets by/i)
+        ) {
+          this.place.pets = true;
+        }
+
+        this.place.propertyId = meta.propertyId;
+        this.place.rooms = meta.propertyInfo.beds;
+        this.place.price = meta.propertyInfo.price;
+        this.place.postcode = meta.location.postcode;
+        this.place.type = meta.propertyInfo.furnishedType;
+
+        this.setPlace(this.place);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+
+    ...mapMutations(["setPlace"])
+  },
+
+  computed: mapState(["url"]),
+
+  watch: {
+    search: "fetch"
+  },
+
+  mounted() {
+    this.fetch();
+  }
+};
 </script>
 
 <template>
-  <div>hey</div>
+  <div></div>
 </template>
 
 <style lang="scss">
-$black: #324048;
 </style>
