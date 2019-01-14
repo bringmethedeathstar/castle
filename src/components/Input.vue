97997700134e4738a23bbb0324cc32dd95c245cb
DaveP <dave@wearecrystal.uk>
2019-01-14 15:52:55 +0000

    run search action on search

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,35 +1,25 @@
 <script>
-import { mapMutations } from "vuex";
+import { mapActions } from "vuex";
 
 export default {
   data() {
     return {
-      address:
-        "https://www.rightmove.co.uk/property-to-rent/property-69395305.html"
+      url: "https://www.rightmove.co.uk/property-to-rent/property-69395305.html"
     };
   },
 
   methods: {
-    search() {
-      // set address in store
-      this.setAddress(this.address);
-
-      // get Host from address
-      this.setHost(
-        this.address
-          .replace(/.*:\/\//, "")
-          .replace("www.", "")
-          .split(".")[0]
-      );
+    doSearch() {
+      this.search(this.url);
     },
 
-    ...mapMutations(["setAddress", "setHost"])
+    ...mapActions(["search"])
   }
 };
 </script>
 
 <template>
-  <input type="text" placeholder="search me" v-model="address" @keyup.enter="search">
+  <input type="text" placeholder="search me" v-model="url" @keyup.enter="doSearch">
 </template>
 
 <style lang="scss">
