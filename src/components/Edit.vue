ff08a6555f8412593d6ac62ef425f29da823ab56
DaveP <dave@wearecrystal.uk>
2019-01-14 18:44:45 +0000

    started work on Markdown editor

---
+++ src/components/Edit.vue
@@ -0,0 +1,39 @@
+<script>
+import { mapState, mapActions } from "vuex";
+import marked from "marked";
+
+export default {
+  data() {
+    return {
+      markdown: "### Description"
+    };
+  },
+
+  computed: {
+    compiled() {
+      return marked(this.markdown, { sanitize: true });
+    },
+
+    ...mapState(["place"])
+  },
+
+  methods: {
+    compile(e) {
+      this.markdown = e.target.value;
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <textarea @input="compile" :value="markdown" name="markdown" rows="10"></textarea>
+
+    <hr>
+
+    <div v-html="compiled"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
